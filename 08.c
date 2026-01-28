fd27d5db27d07881b413d6451ea1b70ac398d45c
Pawan kumar gola <pawankumargola615@gmail.com>
2026-01-28 23:16:08 +0530

    basic_pratic
    c_programing basics pratics

---
+++ 08.c
@@ -0,0 +1,13 @@
+// increment decrement
+#include<stdio.h>
+int main(){
+    int x = 54;
+    printf("%d\n",x);
+    x++; // x = x + 1; // x = 55
+    printf("%d\n",x);
+    x = x * 8; // x = 440
+    printf("%d\n",x);
+    x--; // x = 439
+    printf("%d\n",x);
+    return 0;
+}
