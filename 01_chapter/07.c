fff83ad2d4625327a8eef625d9f32282ef9fc40b
Pawan kumar gola <pawankumargola615@gmail.com>
2026-01-28 23:25:20 +0530

    basic_clear_pratics
    c_programing pratics

---
+++ 01_chapter/07.c
@@ -0,0 +1,11 @@
+// flot data types
+#include<stdio.h>
+int main(){
+    float x = 7;
+    float y = 2;
+    printf("%f\n",x+y);
+    printf("%f\n",x-y);
+    printf("%f\n",x*y);
+    printf("%f\n",x/y);
+    return 0;
+}
---
+++ 01_chapter/09.c.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    float radius = 11;
+    float area = 3.141592*radius*radius;
+    printf("The area is %f",area);
+    return 0;
+}
